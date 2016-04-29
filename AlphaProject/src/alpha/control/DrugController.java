672d9ba39f0f43942417dfe35e0fd22b853040cc
Rune Gangelhof <gangelhof@gmail.com>
2016-04-29 17:29:42 +0200

    Beta
    Igen igen

--- AlphaProject/src/alpha/control/DrugController.java
+++ AlphaProject/src/alpha/control/DrugController.java
@@ -6,8 +6,11 @@
 package alpha.control;
 
 import alpha.interfaces.ICalculateDrugs;
+import alpha.model.Country;
 import alpha.model.Drug;
+import java.util.Calendar;
 import java.util.Random;
+import java.util.TimeZone;
 
 /**
  *
@@ -16,13 +19,40 @@ import java.util.Random;
 public class DrugController implements ICalculateDrugs {
 
     private Random randomizer;
+    Country country = new Country();
+    private String name; // Få navnet på det drug der er valgt??
+    private double price;
+    private int availability;
+    private int priceStrategy;
+    Drug drug = new Drug(name, price, availability, priceStrategy);
 
     public DrugController() {
         this.randomizer = new Random();
     }
     
-    // 0 = Normal, 1 = Old Memory, 2 = CompleteRand, 3 = Clock, 4 = TwoChoice
+    // 0 = Normal (recalculateDrug), 1 = Old Memory, 2 = CompletelyRandom, 3 = Seconds of the Clock, 4 = simpleTwoChoice
+    public void calculateDrug(){
+        
+        switch (priceStrategy){
+            case 0:
+                recalculateDrug(drug);
+                break;
+            case 1:
+                oldMemory();
+                break;
+            case 2:
+                completelyRandom();
+                break;
+            case 3:
+                secondsOfTheClock();
+                break;
+            case 4:
+                simpleTwoChoice();
+                break;
+        }
+    }
 
+    // Normal
     public Drug recalculateDrug(Drug drug) {
         int priceChance = randomizer.nextInt(100);
         if (priceChance < 65) {
@@ -63,5 +93,70 @@ public class DrugController implements ICalculateDrugs {
         }
 
         return newAvailability;
+    }
+    
+    // Simple Two Choice
+    public void simpleTwoChoice()
+    {
+        // Start with the White Price & Amount and then simply switch to the other price every turn.
+        
+        int visit;
+        visit = country.getVisits();
+        
+        // Black price: 300 - black amount: 180
+        if (visit%2==0){
+            drug.setPrice(300);
+            drug.setAvailability(180);
+        } else { // White price: 90 - white amount: 50
+            drug.setPrice(90);
+            drug.setAvailability(50);
+        }
+//        Example: The first time the White Price and White amount is used. The second time the
+//        Black Price and Black Amount must be used. The third time the White Price and White
+//        amount is used and so on.
+    }
+    
+    // Seconds Of The Clock
+    public void secondsOfTheClock()
+    {
+        Calendar cal = Calendar.getInstance( TimeZone.getDefault() );
+        int sec;
+        sec = cal.get(Calendar.SECOND);
+        
+        // Price: Add 1 to the clock if it’s exactly 00 to avoid 0$ price.
+        if(sec==00){
+            sec++;
+            // Price: the double of the current clocks seconds.
+            drug.setPrice(sec*2);
+            // Amount: the current clocks seconds.
+            drug.setAvailability(sec);
+        } else{
+            // Price: the double of the current clocks seconds.
+            drug.setPrice(sec*2);
+            // Amount: the current clocks seconds.
+            drug.setAvailability(sec);
+        }
+
+    }
+    // Completely Random
+    public void completelyRandom()
+    {
+    Random priceRand = new Random();
+    
+    int priceNumber = 0;
+    
+    priceNumber = 10+priceRand.nextInt(12010);
+    
+    Random availabilityRand = new Random();
+    
+    int availabilityNumber = 0;
+    
+    priceNumber = 2+availabilityRand.nextInt(402);
+    
+    }
+    
+    // Old Memory
+    public void oldMemory(){
+        
     }
 }
